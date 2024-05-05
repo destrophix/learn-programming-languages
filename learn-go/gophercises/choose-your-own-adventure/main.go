967345c7bdb097c8ddfddf5c2a2efe2a98f3324f
destrophix <amit.bhagat3301@gmail.com>
2024-05-05 13:02:48 +0530

    Create first page

--- learn-go/gophercises/choose-your-own-adventure/main.go
+++ learn-go/gophercises/choose-your-own-adventure/main.go
@@ -1,29 +1,79 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
-	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request)  {
-	fmt.Println("Request received for root path")
-	io.WriteString(w,"Hello World!!")
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHtmlTemplate))
+}
+
+var tpl *template.Template
+
+var defaultHtmlTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose your own adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{ range .Paragraphs }}
+        <p>{{.}}</p>
+    {{end}}
+    <ul>
+        {{range .Options}}
+            <li><a href="{{.Arc}}">{{.Text}}</a></li>
+        {{end}}
+    </ul> </body>
+</html>
+`
+
+type Story map[string]StoryArc
+
+type StoryArc struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
+}
+
+type handler struct {
+	s Story
+}
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	// http.HandleFunc("/",getRoot)
-	// http.ListenAndServe("localhost:8080",nil)
-	// fmt.Println("exited!!")
-	tmplFile := "home.tmpl"
-	tmpl, err := template.New(tmplFile).ParseFiles(tmplFile)
+	data, err := os.ReadFile("gopher.json")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	err = tmpl.Execute(os.Stdout, "Ram")
+	var story Story
+	err = json.Unmarshal(data, &story)
 	if err != nil {
-		panic(err)
+		log.Fatal("Error during Unmarshal(): ", err)
 	}
+
+	h := NewHandler(story)
+	fmt.Println("Starting server")
+	http.ListenAndServe("localhost:8080", h)
 }
